e91f891ef08c6c902e26bc19cebe36f498ab7a5e
Assshhhhh <9ash09@gmail.com>
2024-03-30 13:35:39 +0500

    Two Way Binding Fixed

--- app/src/main/java/com/example/databinding_with_vm_ld_types/MainViewModel.kt
+++ app/src/main/java/com/example/databinding_with_vm_ld_types/MainViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.databinding_with_vm_ld_types
 
+import androidx.databinding.InverseMethod
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    /*
+    // Cannot use for Two Way Data Binding
     private val quoteLiveDataObject = MutableLiveData<String>("What you give is what you get")
     val quoteLiveData: LiveData<String>
         get() = quoteLiveDataObject
+    */
+
+    val quoteLiveData = MutableLiveData<String>("What you give is what you get")
 
     fun updateQuote() {
-        quoteLiveDataObject.value = "You'll see it when you believe it"
+        quoteLiveData.value = "You'll see it when you believe it"
     }
 
 }
